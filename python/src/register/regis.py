e717d66bd83797a5f8e624d6a7a3c41cdecf38fc
Bishnu Poudel <replytobishnu@gmail.com>
2025-05-17 15:42:11 +0000

    cleaning regis

--- python/src/register/regis.py
+++ python/src/register/regis.py
@@ -35,115 +35,149 @@ def save_file(results, file_object_id):
 def index():
     return render_template("index.html")
 
-@app.route('/login', methods =['GET' ,'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	msg = 'Login page'
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
-		account = cursor.fetchone()
-		cursor.close()
-		if account:
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			session['email'] = account['email']
-			msg = 'Logged in successfully !'+session['username']
-			return render_template('upload.html', msg = msg)
-		else:
-			msg = 'Incorrect username / password !'
-	return render_template('login.html', msg = msg)
+    msg = request.args.get('msg', 'Login page')  
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        
+        try:
+            # Using context manager for database connection and cursor
+            with mysql.connection.cursor(MySQLdb.cursors.DictCursor) as cursor:
+                cursor.execute(
+                    'SELECT * FROM accounts WHERE username = %s AND password = %s',
+                    (username, password,)
+                )
+                account = cursor.fetchone()
+        except Exception as e:
+            msg = f"An error occurred: {str(e)}"
+            return render_template('login.html', msg=msg)
+        
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            session['email'] = account['email']
+            msg = f'Logged in successfully! {session["username"]}'
+            return render_template('upload.html', msg=msg)
+        else:
+            msg = 'Incorrect username / password!'
+    
+    return render_template('login.html', msg=msg)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('loggedin', None)
-	session.pop('id', None)
-	session.pop('username', None)
-	return redirect(url_for('login'))
+    try:
+        session.clear()  
+    except Exception as e:
+        return redirect(url_for('login', msg=f"An error occurred: {str(e)}"))
+    return redirect(url_for('login', msg='Logout successful'))
 
-@app.route('/register', methods =['GET', 'POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-		firstname = request.form['firstname']
-		lastname_middlenames = request.form['lastname_middlenames']
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		elif not username or not password or not email:
-			msg = 'Please fill out the form !'
-		else:
-			cursor.execute('INSERT INTO accounts VALUES (NULL,% s, % s, % s, % s, % s)', (firstname, lastname_middlenames, username, password, email, ))
-			mysql.connection.commit()
-			cursor.close()
-			msg = 'You have successfully registered !'
-			return render_template('upload.html', msg = msg)
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        firstname = request.form['firstname']
+        lastname_middlenames = request.form['lastname_middlenames']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        try:
+            # Using context manager for database connection and cursor
+            with mysql.connection.cursor(MySQLdb.cursors.DictCursor) as cursor:
+                cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+                account = cursor.fetchone()
+        except Exception as e:
+            msg = f"An error occurred: {str(e)}"
+            return render_template('register.html', msg=msg)
+        
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            try:
+                # Using context manager for the INSERT operation
+                with mysql.connection.cursor(MySQLdb.cursors.DictCursor) as cursor:
+                    cursor.execute(
+                        'INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)',
+                        (firstname, lastname_middlenames, username, password, email,)
+                    )
+                    mysql.connection.commit()
+                msg = 'You have successfully registered !'
+                return render_template('upload.html', msg=msg)
+            except Exception as e:
+                msg = f"An error occurred: {str(e)}"
+    
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg=msg)
 
-@app.route('/upload' , methods=['GET','POST'])
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload(msg=None):
     msg = 'Please upload the audio file'
     if request.method == 'POST':
-        fileObjects = request.files.getlist('file')			
-        filenames = [ util.upload(fs_wav, fileObj, session, mysql)  for fileObj in fileObjects ]
-        # Wait till predictions are available
-        predict_str = str( predict.main(user_email = session['email']) )
-        return render_template('download.html', msg = str(len(filenames))+' uploaded for '
-			       +session['email']+' and '+predict_str)
-    return  render_template('upload.html', msg = msg)
+        try:
+            fileObjects = request.files.getlist('file')			
+            filenames = [util.upload(fs_wav, fileObj, session, mysql) for fileObj in fileObjects]
+            # Wait till predictions are available
+            predict_str = str(predict.main(user_email=session['email']))
+            return render_template('download.html', msg=f"{len(filenames)} uploaded for {session['email']} and {predict_str}")
+        except Exception as e:
+            msg = f"An error occurred: {str(e)}"
+    return render_template('upload.html', msg=msg)
+
 
 @app.route('/download', methods=['GET','POST'])
 def download():
 	msg = None
-	if request.method == 'POST':
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		query = "SELECT 1 FROM file_list_24hr WHERE predicted is NULL AND user_email = '{}' LIMIT 1;".format( session['email'])
-		c = cursor.execute(query); counter = 0
-		# async await #TODO:
-		while c > 0:
-			sleep(30)
-			c = cursor.execute(query)
-			counter += 1
-			if counter >= 10:
-				return 'Timed out waiting for predictions, db not updated' , 408
+	# if request.method == 'POST':
+	# 	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+	# 	query = "SELECT 1 FROM file_list_24hr WHERE predicted is NULL AND user_email = '{}' LIMIT 1;".format( session['email'])
+	# 	c = cursor.execute(query); counter = 0
+	# 	# async await #TODO:
+	# 	while c > 0:
+	# 		sleep(30)
+	# 		c = cursor.execute(query)
+	# 		counter += 1
+	# 		if counter >= 10:
+	# 			return 'Timed out waiting for predictions, db not updated' , 408
 		
-		query = "SELECT file_object_id FROM file_list_24hr WHERE predicted=1 AND user_email = '{}';".format( session['email'])
-		c = cursor.execute(query)
-		file_object_list_dict = cursor.fetchall()
-		cursor.close()
-		try:
-			for file_object_dict in file_object_list_dict:
-				file_object_id = file_object_dict['file_object_id']
-				runthis = "curl http://download:8090/download?file_object_id={} --output {}.wav".format(file_object_id,file_object_id)
-				os.system(runthis)					
-		except Exception as e:
-			return render_template('download.html', msg = e)
+	# 	query = "SELECT file_object_id FROM file_list_24hr WHERE predicted=1 AND user_email = '{}';".format( session['email'])
+	# 	c = cursor.execute(query)
+	# 	file_object_list_dict = cursor.fetchall()
+	# 	cursor.close()
+	# 	try:
+	# 		for file_object_dict in file_object_list_dict:
+	# 			file_object_id = file_object_dict['file_object_id']
+	# 			runthis = "curl http://download:8090/download?file_object_id={} --output {}.wav".format(file_object_id,file_object_id)
+	# 			os.system(runthis)					
+	# 	except Exception as e:
+	# 		return render_template('download.html', msg = e)
 		
-		try:
-			# list all .wav files in current folder
-			list_files = [ f for f in os.listdir('.') if f.endswith('.wav') ]
-			# zip all .wav files in current folder
-			with zipfile.ZipFile('out.zip', 'w') as zipMe:        
-				for file in list_files:
-					zipMe.write(file, compress_type=zipfile.ZIP_DEFLATED)
-		except Exception as e:
-			return e
-		return send_file( 'out.zip', download_name='out.zip')
-	else:
-		return render_template('download.html', msg = "Download positive cases")
+	# 	try:
+	# 		# list all .wav files in current folder
+	# 		list_files = [ f for f in os.listdir('.') if f.endswith('.wav') ]
+	# 		# zip all .wav files in current folder
+	# 		with zipfile.ZipFile('out.zip', 'w') as zipMe:        
+	# 			for file in list_files:
+	# 				zipMe.write(file, compress_type=zipfile.ZIP_DEFLATED)
+	# 	except Exception as e:
+	# 		return e
+	# 	return send_file( 'out.zip', download_name='out.zip')
+	# else:
+	# 	return render_template('download.html', msg = "Download positive cases")
+	return render_template('download.html', msg = "Download should receive a POST request")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
