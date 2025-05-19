120823c806711c98ec8d724f5971fd9019030a5c
Bishnu Poudel <replytobishnu@gmail.com>
2025-05-19 12:47:33 +0000

    kubernetes prune, test.py

--- python/src/register/test.py
+++ python/src/register/test.py
@@ -5,32 +5,46 @@ import numpy as np
 # Ensure sample rate
 def ensure_sample_rate(original_sample_rate, waveform, desired_sample_rate=16000):
     if original_sample_rate != desired_sample_rate:
-        desired_length = int(round(len(waveform) / original_sample_rate * desired_sample_rate))
+        desired_length = int(  len(waveform) *  (desired_sample_rate/original_sample_rate ) )
         waveform = resample(waveform, desired_length)
-    return desired_sample_rate, waveform
+    return  waveform
 
 # Read and normalize audio
 def read_and_normalise(file):
     try:
         sample_rate, wav_data = wavfile.read(file, 'rb')
         wav_data = wav_data.mean(axis=1)  # Convert to mono
-        sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
-        wav_data = np.array(wav_data)
+        wav_data = ensure_sample_rate(sample_rate, wav_data)
         waveform = wav_data / wav_data.max()
         return waveform
     except Exception as e:
         raise RuntimeError(f"Error reading and normalizing audio: {str(e)}")
+
+def split_generator(waveform, chunk_size=40000):
+    """
+    Splits the waveform into chunks of specified size.
+    """
+    start_index = 0
+    end_index = len(waveform) - 1 
+    while start_index+chunk_size < end_index:
+        if waveform[start_index] > 0.0:
+            yield waveform[start_index:start_index + chunk_size]
+        start_index += chunk_size
+
+
     
 if __name__ == "__main__":
   import sys, requests, json
   if len(sys.argv) > 1:
     file = sys.argv[1]
   waveform = read_and_normalise(file)
-  waveform = waveform.tolist()
+  waveform = [ chunk.tolist() for chunk in split_generator(waveform)]
+  print(  len(waveform) )
  # Send request to the prediction service
-  url = 'http://snoring:8501/v1/models/snoring_or_not:predict'
-  payload = {"instances": waveform}
-  response = requests.post(url, json=payload)
-  response.raise_for_status()
-  results = json.loads(response.text)['predictions']
-  print(results)
+#   for wave in waveform:
+#     url = 'http://snoring:8501/v1/models/snoring_or_not:predict'
+#     payload = {"instances": wave}
+#     response = requests.post(url, json=payload)
+#     response.raise_for_status()
+#     results = json.loads(response.text)['predictions']
+#     print(results)
